a4d866165af0302d2d7f5c5fb3e1093262cfb0b9
Arthur <arshi@umich.edu>
2017-07-29 16:24:56 -0400

    instagram stuff

---
+++ instagram.js
@@ -0,0 +1,20 @@
+console.log("instagram script loaded");
+
+var token = '5515915196.6e2837d.5cbec0f249fe446b905e84f72cb9d76b',
+    num_photos = 4;
+ 
+$.ajax({
+    url: 'https://api.instagram.com/v1/users/self/media/recent',
+    dataType: 'jsonp',
+    type: 'GET',
+    data: { access_token: token, count: num_photos },
+    success: function(data){
+        console.log(data);
+        for (x in data.data) {
+            $('#instafeed').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>');
+        }
+    },
+    error: function(data){
+        console.log(data);
+    }
+});
